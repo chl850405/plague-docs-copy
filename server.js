026d0d258323da49abc27da9f4c67082d47862cf
insomniKat <ru3ykat@gmail.com>
2022-02-05 17:38:19 -0500

    feature/db and associated server requires plus includes reloc of client side files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const cors = require("cors");
+// const { graphqlHTTP } = require("express-graphql");
+// const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const { typeDefs, resolvers } = require("./server/Schema/index.js");
+// const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true,
+//   })
+// );
+app.use(require("./routes"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/plague-docs-copy",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
