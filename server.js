1a3ce31f02587332990c32242d5f2fe22fdd0e92
chl850405 <christimarchetti@gmail.com>
2022-02-06 16:44:28 -0500

    refactor package.json root

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path")
 // const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 const { typeDefs, resolvers } = require("./server/Schema/index.js");
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,5 +32,11 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
