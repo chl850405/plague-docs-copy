2d76063090569e9d4b9af417a2074600acb3757d
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-06 15:08:55 -0500

    set up graphql

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // const cors = require("cors");
-// const { graphqlHTTP } = require("express-graphql");
-// const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { graphqlHTTP } = require("express-graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const { typeDefs, resolvers } = require("./server/Schema/index.js");
-// const schema = makeExecutableSchema({ typeDefs, resolvers });
+const { typeDefs, resolvers } = require("./server/Schema/index.js");
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema,
-//     graphiql: true,
-//   })
-// );
-app.use(require("./routes"));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+// app.use(require("./routes"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/plague-docs-copy",
