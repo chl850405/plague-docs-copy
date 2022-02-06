ffc12153e3a2bd9c216a3314b3a63757c1c304ea
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-02-06 16:01:58 -0500

    completed page layouts

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
-const Adult = require('./Adult');
-const Child = require('./Child');
-const Adultvaxcard = require('./Adultvaxcard');
-const Childvaxcard = require('./Childvaxcard');
+const Adult = require("./Adult");
+const Child = require("./Child");
+const Adultvaxcard = require("./Adultvaxcard");
+const Childvaxcard = require("./Childvaxcard");
 
 // const Household = require('./Household');
-
 
 module.exports = { Adult, Child, Adultvaxcard, Childvaxcard };
