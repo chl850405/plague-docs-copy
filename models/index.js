1872189575472ff053af8e9be9ff19d35a735399
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-02-19 12:16:52 -0500

    local storage

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const Adult = require("./Adult");
+const Child = require("./Child");
+const Adultvaxcard = require("./Adultvaxcard");
+const Childvaxcard = require("./Childvaxcard");
+
+// const Household = require('./Household');
+
+module.exports = { Adult, Child, Adultvaxcard, Childvaxcard };
