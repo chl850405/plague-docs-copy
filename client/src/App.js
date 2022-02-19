fbe77e9a4812a3b41ac858bedf8474ada86f5338
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-02-19 12:13:51 -0500

    implemented localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router,  Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CreateAcct from "./pages/CreateAcct";
 import AddFamily from "./pages/AddFamily";
@@ -9,14 +9,24 @@ import Login from "./pages/Login";
 import About from "./components/About";
 
 const App = () => {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const handle = () => {
+    localStorage.setItem("Email", email);
+    localStorage.setItem("Password", pwd);
+  };
+  const remove = () => {
+    localStorage.removeItem("");
+    localStorage.removeItem("Password");
+  };
+
   return (
     <>
       <Router>
         <Navbar position="relative">
-          
-            <Route path="/SavedCards" component={SavedCards} />
-            <Route path="/AddFamily" component={AddFamily} />
-          
+          <Route path="/SavedCards" component={SavedCards} />
+          <Route path="/AddFamily" component={AddFamily} />
         </Navbar>
         <About />
         <CreateAcct />
